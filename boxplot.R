6899b1165fdb524505c3c471e712c84a0da851de
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:20:43 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,1 +1,10 @@
 
+
+# Step 5  Genrating different boxplots for population and murder 
+ggplot(data= census_arrests,aes(y=census_arrests$population , x=as.factor(0)))+
+  geom_boxplot()
+
+ggplot(data= census_arrests,aes(y=census_arrests$Murder , x=as.factor(0)))+
+  geom_boxplot()
+
+# Step 6 Histograms are better visualizations as they are easy to read and the histogram is more spreadout.
