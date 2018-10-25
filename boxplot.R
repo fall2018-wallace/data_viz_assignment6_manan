4817a72f4262a49f139bb3970bba6d8992910016
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:03:31 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,1 +1,22 @@
 
+# Step 4 Installing ggplot and Genrating different histograms for population 
+library('ggplot2')
+ggplot(data = census_arrests,aes(census_arrests$population))+
+  geom_histogram(color ='black', fill= 'white',bins = 50)
+
+# Histogram for murder
+ggplot(data = census_arrests,aes(census_arrests$Murder))+
+  geom_histogram(color ='black', fill= 'white', bins = 50)
+
+# Histogram for Rape
+ggplot(data = census_arrests,aes(census_arrests$Rape))+
+  geom_histogram(color ='black', fill= 'white', bins = 50)
+
+# Histogram for UrbanPop
+ggplot(data = census_arrests,aes(census_arrests$UrbanPop))+
+  geom_histogram(color ='black', fill= 'white', bins = 50)
+
+# # Histogram for Assualt
+ggplot(data = census_arrests,aes(census_arrests$Assault))+
+  geom_histogram(color ='black', fill= 'white', bins = 50)
+
