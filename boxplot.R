21cead27fc9a3ce4b48f91bc5ff04983cf8b7df2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:22:24 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -4,7 +4,9 @@
 popboxplot <- ggplot(data= census_arrests,aes(y=census_arrests$population , x=as.factor(0)))+
   geom_boxplot()
 popboxplot
+
 murderboxplot <- ggplot(data= census_arrests,aes(y=census_arrests$Murder , x=as.factor(0)))+
   geom_boxplot()
+murderboxplot
 
 # Step 6 Histograms are better visualizations as they are easy to read and the histogram is more spreadout.
