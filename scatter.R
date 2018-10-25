af96564561cd80a706171aab8410448ee8e7e84c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:36:23 -0400

    Saving scatter.R

--- scatter.R
+++ scatter.R
@@ -1,1 +1,5 @@
 
+# Step 12 Scatterplot
+scatterplot <- ggplot(census_arrests, aes(x = population, y = percentOver18))
+scatterplot <- scatterplot + geom_point(aes(size = Murder, color = Murder))
+scatterplot
