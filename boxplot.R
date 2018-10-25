23eb87167a049628b22faeb89f2ce3bce24864be
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:08:55 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,24 +1,24 @@
 
 # Step 4 Installing ggplot and Genrating different histograms for population 
 library('ggplot2')
-ggplot(data = census_arrests,aes(census_arrests$population))+
-  pophist <- geom_histogram(color ='black', fill= 'white',bins = 50)
+pophist <- ggplot(data = census_arrests,aes(census_arrests$population))+
+   geom_histogram(color ='black', fill= 'white',bins = 50)
 pophist
 
 # Histogram for murder
-ggplot(data = census_arrests,aes(census_arrests$Murder))+
-  murderhist <- geom_histogram(color ='black', fill= 'white', bins = 50)
+murderhist <- ggplot(data = census_arrests,aes(census_arrests$Murder))+
+   geom_histogram(color ='black', fill= 'white', bins = 50)
 murderhist
 
 # Histogram for Rape
-ggplot(data = census_arrests,aes(census_arrests$Rape))+
-  rapehist <- geom_histogram(color ='black', fill= 'white', bins = 50)
+rapehist <- ggplot(data = census_arrests,aes(census_arrests$Rape))+
+   geom_histogram(color ='black', fill= 'white', bins = 50)
 rapehist
 # Histogram for UrbanPop
-ggplot(data = census_arrests,aes(census_arrests$UrbanPop))+
-  urbanpophist <- geom_histogram(color ='black', fill= 'white', bins = 50)
+urbanpophist <- ggplot(data = census_arrests,aes(census_arrests$UrbanPop))+
+   geom_histogram(color ='black', fill= 'white', bins = 50)
 urbanpophist
 # # Histogram for Assualt
-ggplot(data = census_arrests,aes(census_arrests$Assault))+
-  assualthist <- geom_histogram(color ='black', fill= 'white', bins = 50)
+assualthist <- ggplot(data = census_arrests,aes(census_arrests$Assault))+
+   geom_histogram(color ='black', fill= 'white', bins = 50)
 assualthist
